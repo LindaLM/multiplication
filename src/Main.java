10ca3a609bd10641c11ce655c30873b773d79d9e
LindaLM <lindamajerciakova@gmail.com>
2025-04-24 18:02:52 +0200

    method to convert string number to int array

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
         Multiply multiply = new Multiply();
-        System.out.println("multiply with bigInteger: " + multiply.bigIntegerMultiplication("1234567889", "25112344"));
+        try {
+            System.out.println("multiply with bigInteger: " + multiply.bigIntegerMultiplication("098776665", "9786543"));
+        } catch (NumberFormatException e){
+            System.out.println(e);
+        }
+
+        try {
+            ArrayList<Integer> num = multiply.loadNumber("2233fgghgh445678");
+            System.out.println(num.toString());
+        } catch (NumberFormatException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/Multiply.java
+++ src/Multiply.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Multiply {
 
@@ -6,10 +7,28 @@ public class Multiply {
 
     }
     public String bigIntegerMultiplication(String num1, String num2){
-        BigInteger a = new BigInteger(num1);
-        BigInteger b = new BigInteger(num2);
-        BigInteger mult_number = a.multiply(b);
-        return mult_number.toString();
+        try {
+            BigInteger a = new BigInteger(num1);
+            BigInteger b = new BigInteger(num2);
+            BigInteger mult_number = a.multiply(b);
+            return mult_number.toString();
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Wrong format of number");
+        }
+    }
+
+    public ArrayList<Integer> loadNumber(String stringNum){
+        ArrayList<Integer> number = new ArrayList<>(stringNum.length());
+        for (int i = 0; i < stringNum.length(); i++) {
+            char charNum = stringNum.charAt(i);
+            if (Character.isDigit(charNum)) {
+                int intNum = Character.getNumericValue(charNum);
+                number.add(intNum);
+            } else {
+                throw new NumberFormatException("Wrong format of number");
+            }
+        }
+        return number;
     }
 
 }
